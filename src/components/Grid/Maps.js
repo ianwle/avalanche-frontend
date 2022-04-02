f23f7cf02ce11385ca403fe9c30e21ecffee2be6
0x147n <0x147n@gmail.com>
2022-04-02 16:55:09 +0000

    Update Maps.js

--- src/components/Grid/Maps.js
+++ src/components/Grid/Maps.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Map, Marker, Source } from "react-map-gl";
+import { Layer, Map, Marker, Source } from "react-map-gl";
 
 import { connect } from "react-redux";
 // import * as GeneralActions from "@/redux/actions/GeneralActions";
@@ -29,22 +29,25 @@ class Maps extends React.Component {
     render() {
         return (<React.Fragment>
             <Map
-                onClick={ this.handleClick }
+                onClick={this.handleClick}
+                onLoad={() => { }}
                 initialViewState={{
                     longitude: -122.4,
                     latitude: 37.8,
                     zoom: 14
                 }}
                 style={{width: "100vw", height: "100vh"}}
-                mapStyle="mapbox://styles/mapbox/dark-v10"
+                mapStyle="mapbox://styles/mapbox/navigation-guidance-night-v3"
                 mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-                terrain={{ source: 'mapbox-dem', exaggeration: 1.5 }}>
-                <Source
+                // terrain={{ source: 'mapbox-dem', exaggeration: 1.0 }}
+            >
+                {/* <Source
                     id="mapbox-dem"
                     type="raster-dem"
                     url="mapbox://mapbox.mapbox-terrain-dem-v1"
                     tileSize={512}
-                    maxzoom={14}/>
+                    maxzoom={14}
+                /> */}
                 {
                     (() => {
                         let markers = []
@@ -65,6 +68,61 @@ class Maps extends React.Component {
                         })
 
                         return markers
+                    })()
+                }
+
+                {
+                    (() => {
+                        return [(<React.Fragment>
+                            <Layer
+                            id={"add-3d-buildings"}
+                                source={"composite"}
+                                source-layer={"building"}
+                                filter={['==', 'extrude', 'true']}
+                                type={"fill-extrusion"}
+                                minzoom={15}
+                                paint={{
+                                    "fill-extrusion-color": "#aaa",
+                                    "fill-extrusion-height": [
+                                        'interpolate',
+                                        ['linear'],
+                                        ['zoom'],
+                                        15,
+                                        0,
+                                        15.05,
+                                        ['get', 'height']
+                                    ],
+                                    'fill-extrusion-base': [
+                                        'interpolate',
+                                        ['linear'],
+                                        ['zoom'],
+                                        15,
+                                        0,
+                                        15.05,
+                                        ['get', 'min_height']
+                                    ],
+                                    'fill-extrusion-opacity': 0.6
+                                }}>
+                            </Layer>
+                        </React.Fragment>)]
+                    })()
+                }
+
+                {
+                    (() => {
+                        return (
+                <Layer id="sky"
+                    type="sky"
+                    paint={{
+
+                        "sky-type": "atmosphere",
+                        "sky-atmosphere-sun": [0.0, 0.0],
+                        "sky-atmosphere-sun-intensity": 15
+                    }
+                }>
+                                
+                            </Layer>
+                        )
                     })()
                 }
             </Map>
