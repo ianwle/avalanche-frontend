f15be7eac771110c649f5674344112be1a41d266
Ian Wu <iwle@u.nus.edu>
2022-04-02 23:04:06 -0700

    Add drawings capability

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router";
 
 // Components
 import Home from "./Home";
-import Maps from "./Grid/Maps";
-import Menu from "./Widget/Menu";
+// import Maps from "./Grid/Maps";
+// import Menu from "./Widget/Menu";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<Maps />} />
-          <Route path="menu" element={<Menu/>}/>
-        </Routes>
+        <Home />
       </div>
     );
   }
 }
+
